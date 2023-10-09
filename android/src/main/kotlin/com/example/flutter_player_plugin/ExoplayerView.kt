e1347af509555ac0f0e7183f6d133e6307692dfe
Hari <hari@testpress.in>
2023-10-09 16:05:55 +0530

    Update ExoplayerView with latest exoplayer

--- android/src/main/kotlin/com/example/flutter_player_plugin/ExoplayerView.kt
+++ android/src/main/kotlin/com/example/flutter_player_plugin/ExoplayerView.kt
@@ -4,15 +4,12 @@ import android.content.Context
 import android.view.View
 import androidx.media3.common.MediaItem
 import androidx.media3.exoplayer.ExoPlayer
-import androidx.media3.exoplayer.MediaPlayer
-import androidx.media3.exoplayer.MediaPlayerProvider
 import androidx.media3.ui.PlayerView
 import io.flutter.plugin.common.BinaryMessenger
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.platform.PlatformView
-
 
 class ExoPlayerView internal constructor(
     context: Context,
@@ -21,22 +18,23 @@ class ExoPlayerView internal constructor(
 ) :
     PlatformView, MethodCallHandler {
     private val playerView = PlayerView(context)
-    private val exoPlayer = ExoPlayer.Builder(context)
-        .setMediaPlayerProvider(MediaPlayerProvider { MediaPlayer(context) })
-        .build()
+    private val exoPlayer = ExoPlayer.Builder(context).build()
     private val methodChannel: MethodChannel
-    override fun getView(): View {
-        return playerView
-    }
 
     init {
         playerView.player = exoPlayer
-        val mediaItem = MediaItem.Builder().setUri("https://d384padtbeqfgy.cloudfront.net/transcoded/8r65J7EY6NP/video.m3u8").build()
+        val mediaItem = MediaItem.Builder()
+            .setUri("https://d384padtbeqfgy.cloudfront.net/transcoded/8r65J7EY6NP/video.m3u8")
+            .build()
         exoPlayer.setMediaItem(mediaItem)
         exoPlayer.prepare()
         exoPlayer.play()
         methodChannel = MethodChannel(messenger, "plugins.flutter_player_plugin/player_view_$id")
         methodChannel.setMethodCallHandler(this)
+    }
+
+    override fun getView(): View {
+        return playerView
     }
 
     override fun onMethodCall(methodCall: MethodCall, result: MethodChannel.Result) {
@@ -46,11 +44,11 @@ class ExoPlayerView internal constructor(
         }
     }
 
-    private fun setText(methodCall: MethodCall, result: MethodChannel.Result ) {
+    private fun setText(methodCall: MethodCall, result: MethodChannel.Result) {
         val text = methodCall.arguments as String
         result.success(null)
     }
-    
+
     override fun dispose() {
         exoPlayer.release()
     }
